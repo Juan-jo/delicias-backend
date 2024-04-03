79ab82c35ae4e1cd8b889c680750f25f075b7d9a
Juan José Martínez <mtz230396@gmail.com>
2024-04-03 14:27:42 -0600

    aded query to find by Ids

--- src/main/java/com/delivery/app/product/template/repositories/ProductTemplateRepository.java
+++ src/main/java/com/delivery/app/product/template/repositories/ProductTemplateRepository.java
@@ -4,7 +4,12 @@ import com.delivery.app.product.template.models.ProductTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 @Repository
 public interface ProductTemplateRepository extends JpaRepository<ProductTemplate, Integer> {
+
+    List<ProductTemplate> findByIdIn(Collection<Integer> tmplIds);
 
 }
