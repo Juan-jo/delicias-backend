7040f3a3776fb4aa36d626fe8003b6742b49f7dc
jdev <juanj@jdev.com>
2024-06-15 00:01:31 -0600

    add facade authentication

---
+++ src/main/java/com/delivery/app/security/services/AuthenticationFacade.java
@@ -0,0 +1,42 @@
+package com.delivery.app.security.services;
+
+import lombok.AllArgsConstructor;
+import org.keycloak.admin.client.resource.UserResource;
+import org.keycloak.representations.idm.RoleRepresentation;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Component
+public class AuthenticationFacade {
+
+    private static final String ROLE_STORE = "ROLE_STORE";
+    private KeycloakUserService keycloakUserService;
+
+
+    public String userRole() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserResource user = keycloakUserService.getUserResource(authentication.getName());
+
+        return user.roles().realmLevel().listAll().stream()
+                .map(RoleRepresentation::getName)
+                .filter(name -> name.startsWith("ROLE_"))
+                .collect(Collectors.joining());
+    }
+
+    public Integer storeUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserResource user = keycloakUserService.getUserResource(authentication.getName());
+
+        if(userRole().equals(ROLE_STORE)) {
+            if(user.toRepresentation().getAttributes().containsKey("store")) {
+                return Integer.valueOf(user.toRepresentation().getAttributes().get("store").get(0));
+            }
+        }
+
+        return null;
+    }
+}
