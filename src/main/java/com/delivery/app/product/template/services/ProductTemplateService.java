c299f80fac88689228cd7cfe51656e461002ee79
jdev <juanj@jdev.com>
2024-06-18 19:04:41 -0600

    add method create, update and get attr values product tmpl

--- src/main/java/com/delivery/app/product/template/services/ProductTemplateService.java
+++ src/main/java/com/delivery/app/product/template/services/ProductTemplateService.java
@@ -1,16 +1,21 @@
 package com.delivery.app.product.template.services;
 
-import com.delivery.app.product.template.dtos.ProductTemplateDTO;
-import com.delivery.app.product.template.dtos.ProductTemplateReqFilterRowsDTO;
-import com.delivery.app.product.template.dtos.ProductTemplateRowDTO;
+import com.delivery.app.configs.constants.RoleType;
+import com.delivery.app.configs.exception.common.ResourceNotFoundException;
+import com.delivery.app.product.category.model.ProductCategory;
+import com.delivery.app.product.template.dtos.*;
 import com.delivery.app.product.template.models.ProductTemplate;
 import com.delivery.app.product.template.repositories.ProductTemplateRepository;
+import com.delivery.app.product.template.repositories.ViewProductTmplAttrValueRepository;
+import com.delivery.app.restaurant.template.model.RestaurantTemplate;
 import com.delivery.app.security.services.AuthenticationFacade;
 import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
@@ -19,12 +24,65 @@ public class ProductTemplateService {
 
     private final ProductTemplateRepository productTemplateRepository;
     private final AuthenticationFacade authenticationFacade;
+    private final ViewProductTmplAttrValueRepository viewProductTmplAttrValueRepository;
 
     public ProductTemplateDTO findById(Integer tmplId) {
 
         return productTemplateRepository.findById(tmplId)
                 .map(this::modelToProductTemplateDTO)
                 .orElseThrow();
+    }
+
+    @Transactional
+    public ProductTemplateRecordDTO create(
+            ProductTemplateRecordDTO productTemplateRecordDTO
+    ) {
+
+        Integer restaurantId = productTemplateRecordDTO.restaurantId();
+        String userRole = authenticationFacade.userRole();
+
+        if(userRole.equals(RoleType.STORE.value())) {
+            restaurantId = authenticationFacade.storeUser();
+        }
+
+        ProductTemplate productTemplate = productTemplateRepository.save(
+                ProductTemplate.builder()
+                        .name(productTemplateRecordDTO.name())
+                        .category(new ProductCategory(productTemplateRecordDTO.categId()))
+                        .restaurantTmpl(new RestaurantTemplate(restaurantId))
+                        .build()
+        );
+
+        return ProductTemplateRecordDTO.builder()
+                .id(productTemplate.getId())
+                .build();
+    }
+
+    @Transactional
+    public ProductTemplateRecordDTO update(
+            ProductTemplateRecordDTO productTemplateRecordDTO
+    ) {
+
+        ProductTemplate productTemplate = productTemplateRepository.findById(productTemplateRecordDTO.id())
+                .orElseThrow(() -> new ResourceNotFoundException("productTmpl", "id", productTemplateRecordDTO.id()));
+
+        productTemplate.updateBasicInfo(productTemplateRecordDTO);
+
+        return ProductTemplateRecordDTO.builder()
+                .id(productTemplate.getId())
+                .build();
+    }
+
+    public List<ProductTmplAttributeRowDTO> attributeValuesRowDTOS(Integer productTmplId) {
+
+
+        return viewProductTmplAttrValueRepository.findByProductTmplId(productTmplId)
+                .stream().map(r-> ProductTmplAttributeRowDTO.builder()
+                        .id(r.getId())
+                        .attributeValue(r.getNameAttrValue() + " - " +r.getNameAttr())
+                        .sequence(r.getSequenceAttrValue())
+                        .extraPrice(r.getExtraPrice())
+                        .build()).toList();
     }
 
     public Page<ProductTemplateRowDTO> searchFilter(ProductTemplateReqFilterRowsDTO reqFilterRowsDTO){
