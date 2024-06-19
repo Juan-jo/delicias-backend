312fb37c7abed7051d748e07edb96603cfc84cfe
jdev <juanj@jdev.com>
2024-06-18 19:06:49 -0600

    add method get attr by restaurant

---
+++ src/main/java/com/delivery/app/product/template/repositories/ProductTemplateAttributeRepository.java
@@ -0,0 +1,16 @@
+package com.delivery.app.product.template.repositories;
+
+import com.delivery.app.product.attribute.models.ProductAttribute;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductTemplateAttributeRepository extends JpaRepository<ProductAttribute, Integer> {
+
+
+    @Query("SELECT p FROM ProductAttribute p WHERE p.restaurantTmpl.id = :restaurantId ORDER BY p.sequence ASC")
+    List<ProductAttribute> findByRestaurantTmplId(Integer restaurantId);
+}
