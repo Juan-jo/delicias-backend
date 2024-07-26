6ce86d2116aff4bc47485b6426ba64729c005fd2
jdev <juanj@jdev.com>
2024-07-26 14:24:13 -0600

    add mapping update, get, delete

--- src/main/java/com/delivery/app/product/template/controller/ProductTemplateAttributeValueRelController.java
+++ src/main/java/com/delivery/app/product/template/controller/ProductTemplateAttributeValueRelController.java
@@ -1,9 +1,11 @@
 package com.delivery.app.product.template.controller;
 
 import com.delivery.app.configs.validation.common.OnCreate;
+import com.delivery.app.configs.validation.common.OnUpdate;
 import com.delivery.app.product.template.dtos.ProductTemplateAttributeValueDTO;
 import com.delivery.app.product.template.services.ProductTemplateAttributeValueRelService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -21,13 +23,41 @@ public class ProductTemplateAttributeValueRelController {
 
     @PostMapping
     @Validated(OnCreate.class)
-    public ResponseEntity<ProductTemplateAttributeValueDTO> create(
+    public ResponseEntity<?> create(
             @Valid @RequestBody ProductTemplateAttributeValueDTO attributeValueDTO
     ) {
+        templateAttributeValueRelService.create(attributeValueDTO);
 
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping
+    @Validated(OnUpdate.class)
+    public ResponseEntity<?> update(
+            @Valid @RequestBody ProductTemplateAttributeValueDTO attributeValueDTO
+    ) {
+        templateAttributeValueRelService.update(attributeValueDTO);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    @Validated(OnUpdate.class)
+    public ResponseEntity<ProductTemplateAttributeValueDTO> update(
+            @NotNull @PathVariable Integer id
+    ) {
         return ResponseEntity.ok(
-                templateAttributeValueRelService.create(attributeValueDTO)
+                templateAttributeValueRelService.findById(id)
         );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(
+            @NotNull @PathVariable Integer id
+    ) {
+
+        templateAttributeValueRelService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
