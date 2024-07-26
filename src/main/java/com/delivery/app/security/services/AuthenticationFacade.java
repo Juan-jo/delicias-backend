c0b143603c2ebf513c6c5da82a5d5e273ef2ed4a
jdev <juanj@jdev.com>
2024-07-26 16:11:26 -0600

    add method get user id

--- src/main/java/com/delivery/app/security/services/AuthenticationFacade.java
+++ src/main/java/com/delivery/app/security/services/AuthenticationFacade.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
@@ -38,5 +39,11 @@ public class AuthenticationFacade {
         }
 
         return null;
+    }
+
+    public UUID userId() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return UUID.fromString(authentication.getName());
     }
 }
