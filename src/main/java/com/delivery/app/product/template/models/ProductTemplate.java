09118eefc18e3d7fc5848bd34a6df744e15338b6
jdev <juanj@jdev.com>
2024-07-26 14:16:33 -0600

    Add map picture path

--- src/main/java/com/delivery/app/product/template/models/ProductTemplate.java
+++ src/main/java/com/delivery/app/product/template/models/ProductTemplate.java
@@ -56,6 +56,8 @@ public class ProductTemplate extends AuditableEntity {
 
     private Boolean active;
 
+    private String picture;
+
     @OneToMany(mappedBy = "id.productTemplate")
     private Set<ProductAttributeProductTemplateRel> attributeProductTemplateRels;
 
