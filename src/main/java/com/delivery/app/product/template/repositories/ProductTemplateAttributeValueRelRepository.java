86567a9b62bdb9fd3f8d3b011e564bf9631fa064
jdev <juanj@jdev.com>
2024-07-26 14:23:10 -0600

    add method get attributes value

--- src/main/java/com/delivery/app/product/template/repositories/ProductTemplateAttributeValueRelRepository.java
+++ src/main/java/com/delivery/app/product/template/repositories/ProductTemplateAttributeValueRelRepository.java
@@ -2,9 +2,17 @@ package com.delivery.app.product.template.repositories;
 
 import com.delivery.app.product.template.models.ProductTemplateAttributeValueRel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProductTemplateAttributeValueRelRepository extends JpaRepository<ProductTemplateAttributeValueRel, Integer> {
+
+    @Modifying
+    void deleteByAttributeValueId(Integer id);
+
+    Optional<ProductTemplateAttributeValueRel> findByTemplateIdAndAttributeValueId(Integer productTmplId, Integer attrValueId);
 
 }
