90f993887fe3fc34dd6962e0f3ab97ddbb121358
jdev <juanj@jdev.com>
2024-07-27 23:09:23 -0600

    add mobile create order DTO

---
+++ src/main/java/com/delivery/app/mobile/dtos/MobileCreateOrderDTO.java
@@ -0,0 +1,54 @@
+package com.delivery.app.mobile.dtos;
+
+import com.delivery.app.configs.validation.common.OnCreate;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+public record MobileCreateOrderDTO(
+
+        @NotNull(message = "The parameter is mandatory.", groups = { OnCreate.class })
+        Integer restaurantId,
+
+        @NotNull(message = "The parameter is mandatory.", groups = { OnCreate.class })
+        Double subtotal,
+
+        @NotNull(message = "The parameter is mandatory.", groups = { OnCreate.class })
+        Double discount,
+
+        @NotNull(message = "The parameter is mandatory.", groups = { OnCreate.class })
+        Double costService,
+
+        @NotNull(message = "The parameter is mandatory.", groups = { OnCreate.class })
+        Double total,
+
+        @Size(max = 250, message = "Not valid name. Must have minimum 1 chars or maximum 150 chars.", groups = { OnCreate.class})
+        String notes,
+
+        @Valid
+        @NotEmpty(message = "The parameter is mandatory.", groups = { OnCreate.class })
+        List<ProductTmpl> productTmpl
+) {
+
+    public record ProductTmpl(
+
+            @NotNull(message = "The parameter is mandatory.", groups = { OnCreate.class })
+            Integer id,
+
+            @NotNull(message = "The parameter is mandatory.", groups = { OnCreate.class })
+            Integer qty,
+
+            @Valid
+            @NotEmpty(message = "The parameter is mandatory.", groups = { OnCreate.class })
+            List<AttrValue> attrValues
+    ) { }
+
+
+    public record AttrValue(
+            @NotNull(message = "The parameter is mandatory.", groups = { OnCreate.class })
+            Integer id
+    ) { }
+}
