097666dd5bbd1778f26e9fb76943074e55833a5c
jdev <juanj@jdev.com>
2024-07-30 17:43:31 -0600

    producer refactor topic and DTO

---
+++ src/main/java/com/delivery/app/kafka/producer/KafkaTopicKanbanProducer.java
@@ -0,0 +1,27 @@
+package com.delivery.app.kafka.producer;
+
+import com.delicias.kafka.core.dto.KafkaTopicKanbanDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaTopicKanbanProducer {
+
+    private static final String TOPIC = "kanban";
+
+    @Autowired
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public KafkaTopicKanbanProducer(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessageTopicKanban(KafkaTopicKanbanDTO message) {
+        kafkaTemplate.send(TOPIC, message);
+    }
+
+    // TODO multiple send DTO in TOPIC (Config DTO -> com.delicias.kafka.core.dto)
+    // public void sendMessageTodo(KafkaTodoDTO message) { kafkaTemplate.send("test", message); }
+
+}
