66915a8256904467e8c8d72d03f3750acf792c86
jdev <juanj@jdev.com>
2024-08-12 15:48:03 -0600

    add component delicias config, add upload file component

---
+++ src/main/java/com/delivery/app/configs/DeliciasAppProperties.java
@@ -0,0 +1,31 @@
+package com.delivery.app.configs;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "delicias")
+@Getter
+@Setter
+public class DeliciasAppProperties {
+
+    String timezone;
+    private CloudFiles files;
+
+    @Getter
+    @Setter
+    public static class CloudFiles {
+        private String coverSize;
+        private String server;
+        private UploadFile upload;
+
+        @Getter
+        @Setter
+        public static class UploadFile {
+            private String path;
+        }
+    }
+
+}
---
+++ src/main/java/com/delivery/app/utils/DeliciasFileUtils.java
@@ -0,0 +1,46 @@
+package com.delivery.app.utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Component
+public class DeliciasFileUtils {
+
+    @Value("${delicias.files.upload.path}")
+    private Path fileStorageLocation;
+
+    public String saveFile(MultipartFile file) throws IOException {
+
+        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+
+        Path targetLocation = this.fileStorageLocation.resolve(fileName);
+
+        verifyDirectoryExistence(targetLocation);
+        Files.copy(file.getInputStream(), targetLocation);
+
+        return fileName;
+    }
+
+
+
+    public void deleteFile(String fileName) {
+        try {
+            Files.deleteIfExists(fileStorageLocation.resolve(fileName).normalize());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void verifyDirectoryExistence(Path targetLocation) throws IOException {
+        if (!Files.exists(fileStorageLocation)) {
+            Files.createDirectories(fileStorageLocation);
+        }
+    }
+
+}
