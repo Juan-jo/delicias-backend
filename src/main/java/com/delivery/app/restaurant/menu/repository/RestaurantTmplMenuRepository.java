362d7cbc955fd0e74c3574ae87c85232cc0fdd2f
jdev <juanj@jdev.com>
2024-08-14 17:49:46 -0600

    modify query order by sequence

--- src/main/java/com/delivery/app/restaurant/menu/repository/RestaurantTmplMenuRepository.java
+++ src/main/java/com/delivery/app/restaurant/menu/repository/RestaurantTmplMenuRepository.java
@@ -1,6 +1,7 @@
 package com.delivery.app.restaurant.menu.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.delivery.app.restaurant.menu.model.RestaurantTmplMenu;
 
@@ -10,6 +11,7 @@ import java.util.List;
 public interface RestaurantTmplMenuRepository extends JpaRepository<RestaurantTmplMenu, Integer> {
 
 
+    @Query("SELECT m FROM RestaurantTmplMenu m  WHERE m.restaurantTmpl.id = :restaurantId ORDER BY m.sequence ASC")
     List<RestaurantTmplMenu> findByRestaurantTmplId(Integer restaurantId);
 
 }
